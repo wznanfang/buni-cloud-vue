5b919a026605666b5549190554b922b914015193
不逆 <wzp9215@qq.com>
2024-08-30 11:09:32 +0800

    feat: AES解密方法

--- src/baseConfig/secret.js
+++ src/baseConfig/secret.js
@@ -6,6 +6,9 @@ const iv = '1234567890123456'; // 偏移量
 
 /**
  * AES加密 ：字符串 key iv  返回base64
+ * @param text
+ * @returns {string}
+ * @constructor
  */
 export function Encrypt(text) {
     const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(text), CryptoJS.enc.Utf8.parse(key), {
@@ -14,4 +17,19 @@ export function Encrypt(text) {
         padding: CryptoJS.pad.Pkcs7
     });
     return encrypted.toString();
+}
+
+/**
+ * AES解密 ：字符串 key iv
+ * @param text
+ * @returns {*}
+ * @constructor
+ */
+export function Decrypt(text) {
+    const bytes = CryptoJS.AES.decrypt(text, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(iv),
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    return CryptoJS.enc.Utf8.stringify(bytes);
 }
